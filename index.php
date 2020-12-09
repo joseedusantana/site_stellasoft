fe3b87cb80c8cabb06620b42b16891161b422e54
joseedusantana <37674403+joseedusantana@users.noreply.github.com>
2020-12-09 14:08:07 -0300

    Criando

---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viemport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
+        <!-- LINK -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/style04A.css">
+        <script type="text/jscript" src="assets/js/jquery-3.3.1.min.js"></script>
+        <script type="text/jscript" src="assets/js/bootstrap.bundle.min.js"></script>
+        <script type="text/javascript" src="assets/js/jquery.mask.min.js"></script>
+        <script type="text/javascript" src="assets/js/jquery.validate.min.js"></script>
+        <script type="text/javascript" src="assets/js/additional-methods.min.js"></script>
+        <script type="text/jscript" src="assets/js/localization/messages_pt_BR.js"></script>
+        
+        <title>SITE B7Web</title>
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="titulo">
+                <h1>Site StellaSoft</h1>
+            </div>
+            
+            <a href="../SITE_MENU_GERAL/index2.php" class="btn btn-danger btn-lg btn-block" role="button" aria-pressed="true"><b>Voltar</b></a>
+
+            <section class="layout">    
+                <div class="item a">
+                    <h4>PHP - Nivel 1</h4>
+                    <p>Exemplos e Exercicios</p>
+                    <a href="_paginas/menu_php-mod.php" id="botao" class="btn btn-primary btn-md" role="button" aria-pressed="true" >Entrar</a>
+                </div>
+                <div class="item b">
+                    <img src="assets/images/codeLogoA.png" alt="">
+                </div>
+                
+<!--                 <div class="item d">
+                    <h4>Node e MongoDB Nivel 1</h4>
+                    <p>Exemplos e Exercicios</p>
+                    <a href="" id="botao" class="btn btn-primary btn-md" role="button" aria-pressed="true" >Entrar</a>
+                </div> -->
+<!--                 <div class="item e">
+                    <h4>Lavarel 6 Nivel 1</h4>
+                    <p>Exemplos e Exercicios</p>
+                    <a href="" id="botao" class="btn btn-primary btn-md" role="button" aria-pressed="true" >Entrar</a>
+                </div> -->
+                <!-- <div class="item f">
+                    <h4>PHP do Zero ao Profissional</h4>
+                    <p>Exemplos e Exercicios</p>
+                    <a href="" id="botao" class="btn btn-primary btn-md" role="button" aria-pressed="true" >Entrar</a>
+                </div>
+                <div class="item g">
+                    <h4>WordPress do Zero ao Profissional</h4>
+                    <p>Exemplos e Exercicios</p>
+                    <a href="" id="botao" class="btn btn-primary btn-md" role="button" aria-pressed="true" >Entrar</a>
+                </div> -->
+                <div class="item h">
+                    <h4>HTML5 e CSS3 1</h4>
+                    <p>Exemplos e Exercicios</p>
+                    <a href="" id="botao" class="btn btn-primary btn-md" role="button" aria-pressed="true" >Entrar</a>
+                </div>
+               
+               
+               
+            </section>
+            
+            
+        </div>
+        
+    </body>
+</html>
